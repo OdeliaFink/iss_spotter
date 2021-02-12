57e1d5e9a3ed49c780f55470e68c9f2bcc8a8f39
OdeliaFink <odelia.finkel@gmail.com>
2021-02-12 01:58:27 +0000

    redone with promises

---
+++ index2.js
@@ -0,0 +1,16 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+  //body will now be the response body (JSON string) 
+  //returned from our second API call, to Free Geo IP, containing geographical information for the given IP address.
+
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
